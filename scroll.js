d2a743b1b3b1f16000de59328944ad708e63d257
ira-22 <85496191+ira-22@users.noreply.github.com>
2024-02-20 17:16:10 +0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,19 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+    if(pos>80){
+        scrollProgress.style.display = "grid";
+    }
+    else {
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    })
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
